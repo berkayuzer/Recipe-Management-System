b4ec1565a355d6c8467a5c590084b5d428e6c081
UcanSosis <alperen@Alperen-MacBook-Air.local>
2024-05-11 18:43:21 +0300

    yorum

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,15 +1,11 @@
 package src;
 
-import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Recipe {
-    List<String> ingredients = new ArrayList<>();
-    List<String> categories = new ArrayList<>();
-    List<String> tags = new ArrayList<>();
-    String recipeName;
-    String cookingInstructions;
+    List<String> categories = new ArrayList<>(), tags = new ArrayList<>(), ingredients = new ArrayList<>();
+    String recipeName, cookingInstructions;
     int servingSize;
     float avgRating;
 
