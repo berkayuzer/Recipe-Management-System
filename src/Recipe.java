184bde00a4ba8381318b36e3d1ca27e8dd92638e
erirem <eriremerirem@gmail.com>
2024-05-11 19:15:34 +0300

    Recipe v1.0

--- src/Recipe.java
+++ src/Recipe.java
@@ -2,9 +2,10 @@ package src;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.SplittableRandom;
 
 public abstract class Recipe {
-    List<String> categories = new ArrayList<>(), tags = new ArrayList<>(), ingredients = new ArrayList<>();
+    List<String> categories, tags, ingredients;
     String recipeName, cookingInstructions;
     int servingSize;
     float avgRating;
@@ -16,6 +17,39 @@ public abstract class Recipe {
         this.tags = tags;
         this.avgRating = avgRating;
     }
+    void createRecipe(String recipeName){
+    this.recipeName = recipeName;
+    }
+    List<String> getIngredients(){
+    return ingredients;
+    }
+    void setIngredients(List <String> ingredients){
+        this.ingredients = ingredients;
+    }
+     String getInstructions(){
+        return cookingInstructions;
+    }
+    void setInstructions(String cookingInstructions){
+    this.cookingInstructions = cookingInstructions;
+    }
+    int getServingSize(){
+        return servingSize;
+    }
+    void setCategories(List<String> categories){
+        this.categories = categories;
+    }
+    List<String> getCategories(){
+        return categories;
+    }
+    void setTags(List<String> tags){
+        this.tags=tags;
+    }
 
+    List<String> getTags(){
+        return tags;
+    }
+    void setAvgRating(float avgRating){
+        this.avgRating=avgRating;
+    }
 }
 
