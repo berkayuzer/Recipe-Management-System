bfcb57e4ddfe7e3011f418f2bd3030df5f3164b7
erirem <eriremerirem@gmail.com>
2024-05-11 18:33:47 +0300

    Recipe class deneme

--- Recipe.java
+++ Recipe.java
@@ -1,8 +1,17 @@
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Recipe {
-    List<String> ingredients = new ArrayList<String>();
+    List<String> ingredients = new ArrayList<>();
+    List<String> categories = new ArrayList<>();
+    List<String> tags = new ArrayList<>();
+    String recipeName;
+    String cookingInstructions;
+    int servingSize;
+    float avgRating;
+
+
 
 
 }
