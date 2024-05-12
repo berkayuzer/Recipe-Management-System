f25f2918a67f8844060c489f5caaf194b62b9002
erirem <eriremerirem@gmail.com>
2024-05-12 22:59:11 +0300

    Recipe v2.0

--- src/RecipeClient.java
+++ src/RecipeClient.java
@@ -1,10 +1,18 @@
 package src;
 
 import java.util.Scanner;
+import java.util.SortedMap;
 
 public class RecipeClient {
 public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
     System.out.println("Main Menu:\n *select the index of the option that you wanna use* \n"+"1.Create Recipe\n"+"2.Search Recipe\n"+"3.Modify Recipe\n"+"4.Rate Recipe");
+    int index = scanner.nextInt();
+    if (index == 1){
+        System.out.println("Create Recipe Menu");
+        System.out.println("Enter the recipe name:");
+        String recipeName = scanner.nextLine();
+        System.out.println("");
+        }
     }
 }
